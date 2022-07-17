c1d0d563589ecb35a0e59f628f26a9ce976cb5c3
user.email <eng_maythem_84@yahoo.com>
2022-07-17 21:45:14 +0300

    Dr. MKA's WhatsApp PA

---
+++ main.js
@@ -0,0 +1,41 @@
+// const wbm = require('wbm');
+
+// wbm.start().then(async () => {
+//     const phones = ['9647725487690'];
+//     var message;
+//     for (let i = 0; i < 3; i++) {
+//         message = 'Good Morning ' + i;
+//         await wbm.send(phones, message);
+//         console.log("To " + phones + ": " + message);
+//     }
+//     // await wbm.end();
+// }).catch(err => console.log(err));
+
+const { Client } = require('whatsapp-web.js');
+const QR = require('qrcode-terminal');
+
+const client = new Client();
+client.initialize();
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    // console.log('QR RECEIVED', qr);
+    QR.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+
+    // REPLYING to an incoming message
+    var Authors = new Array();
+    client.on('message', msg => {
+        // if (msg.body == 'ping' && !Authors.includes(msg.from.toString)) {
+        if (!Authors.includes(msg.from.toString())) {
+            // if (true) {
+            msg.reply('Hi, I am Sandra, Dr. MKA\'s PA\n Dr. MKA is busy right now, Shall I take a note from you?');
+            // msg.reply('Automates REPLY Message!');
+            // client.sendMessage(msg.chatId.toString(), 'How can I help you? Oh sorry, I forgot that I am a robot, I cannot really be of help!!! Sorry!');
+            Authors.push(msg.from.toString());    // Add the serialized phone number to the array
+        }
+        console.log("Authors are: " + Authors);
+    });
+});
